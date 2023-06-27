5a4c8bf8136045f84104333a1228ff372ddb0d17
devedium <devedium@gmail.com>
2023-06-27 17:17:53 -0400

    rate limiting

--- Program.cs
+++ Program.cs
@@ -2,15 +2,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.RateLimiting;
 
 namespace RLTExample
 {
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -36,7 +46,7 @@ namespace RLTExample
                         IssuerSigningKey = new SymmetricSecurityKey(
                             Encoding.UTF8.GetBytes(securityKey))
                     };
-                });
+                });            
 
             builder.Services.AddAuthorization();
 
@@ -46,22 +56,100 @@ namespace RLTExample
                 options.AddPolicy("admin", policy => policy.RequireRole("admin").RequireClaim("scope", "api"));
             });
 
-            var app = builder.Build();
+            builder.Services.AddRateLimiter(limiterOptions =>
+            {
+                limiterOptions.OnRejected = (context, cancellationToken) =>
+                {
+                    if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    {
+                        context.HttpContext.Response.Headers.RetryAfter =
+                            ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+                    }
+
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;                    
+
+                    return new ValueTask();
+                };
+
+                limiterOptions.AddPolicy("user_get", context =>
+                {
+                    var username = "anonymous user";
+                    if (context.User.Identity?.IsAuthenticated is true)
+                    {
+                        username = context.User.Identity.Name;
+                    }
+
+                    return RateLimitPartition.GetSlidingWindowLimiter(username,
+                        _ => new SlidingWindowRateLimiterOptions
+                        {
+                            PermitLimit = 10,
+                            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                            QueueLimit = 4,
+                            Window = TimeSpan.FromSeconds(20),
+                            SegmentsPerWindow = 4
+                        });
+                });
+
+                limiterOptions.AddPolicy("user_post", context =>
+                {
+                    var username = "anonymous user";
+                    if (context.User.Identity?.IsAuthenticated is true)
+                    {
+                        username = context.User.Identity.Name;
+                    }
+
+                    return RateLimitPartition.GetSlidingWindowLimiter(username,
+                        _ => new SlidingWindowRateLimiterOptions
+                        {
+                            PermitLimit = 4,
+                            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                            QueueLimit = 2,
+                            Window = TimeSpan.FromSeconds(20),
+                            SegmentsPerWindow = 4
+                        });
+                });
+
+                limiterOptions.AddPolicy("admin", context =>
+                {
+                    return RateLimitPartition.GetNoLimiter("admin");
+                });
+
+                limiterOptions.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+                {
+                    return RateLimitPartition.GetConcurrencyLimiter("global", _ => new ConcurrencyLimiterOptions
+                    {
+                        PermitLimit = 20,
+                        QueueLimit = 6,
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                    }); 
+                });
+            });
+
+
+            var app = builder.Build();            
 
             app.UseAuthentication();
             app.UseAuthorization();
+            
+            app.UseRateLimiter();
 
             app.MapGet("/", (() => "Hello, World!"));
 
             app.MapGet("/customer", () =>
             {
-                return new List<dynamic>
+                return new List<Customer>
                 {
-                    new {Id = 1, Name = "John Doe"},
-                    new {Id = 2, Name = "Jane Smith"},
-                    new {Id = 3, Name = "Bob Johnson"}
+                    new Customer{Id = 1, Name = "John Doe"},
+                    new Customer{Id = 2, Name = "Jane Smith"},
+                    new Customer{Id = 3, Name = "Bob Johnson"}
                 };
-            }).RequireAuthorization("user");
+            }).RequireAuthorization("user").RequireRateLimiting("user_get");
+
+            app.MapPost("/customer", (Customer newCustomer) =>
+            {   
+                return Results.Created($"/customer/{newCustomer.Id}", newCustomer); 
+
+            }).RequireAuthorization("user").RequireRateLimiting("user_post");
 
             app.MapGet("/user", () =>
             {
@@ -71,7 +159,7 @@ namespace RLTExample
                     new {Id = 2, Name = "Emily Elizabeth"},
                     new {Id = 3, Name = "Michael David"}
                 };
-            }).RequireAuthorization("admin");
+            }).RequireAuthorization("admin").RequireRateLimiting("admin");
 
 
             app.MapPost("/token", async (HttpContext httpContext) =>
